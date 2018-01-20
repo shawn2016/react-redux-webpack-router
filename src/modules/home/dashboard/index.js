d753de3d6bfd9ff51b6997e24856788db736923d
shawn <787990963@qq.com>
2018-01-20 23:30:12 +0800

    test

--- src/modules/home/dashboard/index.js
+++ src/modules/home/dashboard/index.js
@@ -4,7 +4,7 @@ class Dashboard extends React.Component {
   render() {
     return (
       <div>
-        Dashboard2<img src={home_bg} alt="tupian" />
+        Dashboard<img src={home_bg} alt="tupian" />
       </div>
     )
   }
