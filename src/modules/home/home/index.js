12bf474c62461b24c19e461a3b9017753307a7af
shawn <787990963@qq.com>
2018-02-22 23:56:42 +0800

    test

--- src/modules/home/home/index.js
+++ src/modules/home/home/index.js
@@ -42,7 +42,7 @@ class Home extends React.Component {
         {this.state.dataone ? this.state.dataone.isCfcaUser : ''}<br /><br />
         {this.state.dataone ? this.state.dataone.phonenum : ''}<br /><br />
         {this.state.dataone ? this.state.dataone.userCertImgDown : ''}<br /><br />
-        <br />0999999999999999999999<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <br />099999999999999999999<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         <div>
           <ScrollToTop showUnder={160} style={{ bottom: 20, zIndex: 999 }}>
             <div>Top</div>
