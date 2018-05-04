53f9a5e0319297404f7274cc918305bb29ff4b2e
shawn <787990963@qq.com>
2018-05-04 12:13:42 +0800

    升级webpack4

--- webpack.config.dev.babel.js
+++ webpack.config.dev.babel.js
@@ -1,94 +1,111 @@
 import webpack from 'webpack'
 import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-import ExtractTextPlugin from "extract-text-webpack-plugin"
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 import devApiConfig from './src/config/dev.env'
 import testApiConfig from './src/config/test.env'
 import prodApiConfig from './src/config/prod.env'
 const extractSass = new ExtractTextPlugin({
-    filename: "[name].[contenthash].css",
-    disable: process.env.NODE_ENV === "development"
-});
+  filename: '[name].css',
+  disable: process.env.NODE_ENV === 'development',
+})
 module.exports = {
-    context: path.resolve(__dirname, './src'),
-    entry: [
-        'babel-polyfill',
-        path.join(__dirname, './src/app.js')
-    ],
-    output: {
-        filename: '[name].js',
-        path: path.join(__dirname, './dist'),
-        publicPath: '/'
-    },
-    devtool: "source-map",
-    module: {
-        rules: [{
-            enforce: 'pre',
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'eslint-loader'
-        },
-        {
-            test: /\.(less|css)$/,
-            use: extractSass.extract({
-                use: ['style-loader', 'css-loader', 'less-loader']
-            })
-        },
-        {
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader'
-            }
-        }, {
-            test: /\.scss$/,
-            use: extractSass.extract({
-                use: [{
-                    loader: "css-loader"
-                }, {
-                    loader: "sass-loader"
-                }],
-                // use style-loader in development 
-                fallback: "style-loader"
-            })
-        },
-        {
-            test: /\.(png|svg|jpg|gif)$/,
-            loader: 'url-loader?limit=80000&name=imgs/[hash].[ext]'
-        },
-        {
-            test: /\.(woff|woff2|eot|ttf)$/i,
-            loader: 'url-loader?limit=80000&name=fonts/[hash].[ext]'
-        }
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.md', '.txt'],
-        alias: {
-            modules: path.resolve(__dirname, './src/modules'),
-            reduxes: path.resolve(__dirname, './src/reduxes'),
-            utils: path.resolve(__dirname, './src/utils'),
-            routers: path.resolve(__dirname, './src/routers'),
-            assets: path.resolve(__dirname, './src/assets'),
-        }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: path.join(__dirname, './src/index.html') }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new webpack.DefinePlugin({
-            NODE_ENV: process.env.NODE_ENV,
-            __ENV__: process.env.NODE_ENV == 'production' ? prodApiConfig : process.env.NODE_ENV == 'development' ? devApiConfig : process.env.NODE_ENV == 'test' ? testApiConfig : devApiConfig
+  context: path.resolve(__dirname, './src'),
+  mode: 'development',
+  entry: ['babel-polyfill', path.join(__dirname, './src/app.js')],
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, './dist'),
+    publicPath: '/',
+  },
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.(less|css)$/,
+        use: extractSass.extract({
+          use: ['style-loader', 'css-loader', 'less-loader'],
         }),
-        extractSass
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+          // use style-loader in development
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'url-loader?limit=80000&name=imgs/[hash].[ext]',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)$/i,
+        loader: 'url-loader?limit=80000&name=fonts/[hash].[ext]',
+      },
     ],
-    devServer: {
-        hot: true,
-        host: '0.0.0.0',
-        port: 8090,
-        publicPath: '/',
-        contentBase: path.resolve(__dirname, './src'),
-        historyApiFallback: true,
-        disableHostCheck: true,
-    }
+  },
+  resolve: {
+    extensions: ['.js', '.md', '.txt'],
+    alias: {
+      modules: path.resolve(__dirname, './src/modules'),
+      reduxes: path.resolve(__dirname, './src/reduxes'),
+      utils: path.resolve(__dirname, './src/utils'),
+      routers: path.resolve(__dirname, './src/routers'),
+      assets: path.resolve(__dirname, './src/assets'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.join(__dirname, './src/index.html') }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      NODE_ENV: process.env.NODE_ENV,
+      __ENV__:
+        process.env.NODE_ENV === 'production'
+          ? prodApiConfig
+          : process.env.NODE_ENV === 'development'
+            ? devApiConfig
+            : process.env.NODE_ENV === 'test'
+              ? testApiConfig
+              : devApiConfig,
+    }),
+    extractSass,
+  ],
+  devServer: {
+    hot: true,
+    host: '0.0.0.0',
+    port: 8090,
+    publicPath: '/',
+    contentBase: path.resolve(__dirname, './src'),
+    historyApiFallback: true,
+    disableHostCheck: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
+      },
+    },
+  },
 }
--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -7,107 +7,135 @@ import devApiConfig from './src/config/dev.env'
 import testApiConfig from './src/config/test.env'
 import prodApiConfig from './src/config/prod.env'
 const extractSass = new ExtractTextPlugin({
-    filename: "../assets/styles.[chunkhash:8].css",
-    disable: process.env.NODE_ENV === "development"
-});
+  filename: '../assets/styles.[chunkhash:8].css',
+  disable: process.env.NODE_ENV === 'development',
+})
 
 module.exports = {
-    context: path.resolve(__dirname, './'),
-    entry: [
-        'babel-polyfill',
-        path.resolve(__dirname, './src/app.js')
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist/assets'),
-        publicPath: '../assets/',
-        filename: '[name].[chunkhash:8].js',
-        chunkFilename: '[name].[chunkhash:8].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }, {
-                test: /\.scss$/,
-                use: extractSass.extract({
-                    use: [{
-                        loader: "css-loader"
-                    }, {
-                        loader: "sass-loader"
-                    }],
-                    // use style-loader in development 
-                    fallback: "style-loader"
-                })
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                loader: 'url-loader?limit=80000&name=imgs/[chunkhash:8].[ext]'
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf)$/i,
-                loader: 'url-loader?limit=80000&name=fonts/[chunkhash:8].[ext]'
-            }
-        ]
-    },
-    plugins: [ // 插件
-        extractSass,
-        // new webpack.HotModuleReplacementPlugin(),
-        // new webpack.NamedModulesPlugin(),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks: (module) => (
-                // 该配置假定你引入的 vendor 存在于 node_modules 目录中
-                module.context && module.context.indexOf('node_modules') !== -1
-            )
-        }),
-        new CompressionPlugin({
-            asset: "[path].gz[query]",
-            algorithm: "gzip",
-            test: /\.(js|html)$/,
-            threshold: 10240,
-            minRatio: 0.8
-        }),
-        new ExtractTextPlugin('styles.[chunkhash:8].css'),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, './src/index.html')
-        }),
-        // 压缩JS代码,CSS 没有被压缩到
-        new webpack.optimize.UglifyJsPlugin({
-            output: {
-                comments: false,
-            },
-            compress: {
-                warnings: false,
-                drop_debugger: true,
-                drop_console: true
-            },
-        }),
-        new webpack.IgnorePlugin(/vertx/),
-        new webpack.DefinePlugin({
-            NODE_ENV: process.env.NODE_ENV,
-            __ENV__: process.env.NODE_ENV == 'production' ? prodApiConfig : process.env.NODE_ENV == 'development' ? devApiConfig : process.env.NODE_ENV == 'test' ? testApiConfig : devApiConfig
-        }),
-    ],
-    resolve: {
-        extensions: ['.js', '.md', '.txt'],
-        alias: {
-            'react-robotUI': path.resolve(__dirname, './react-robotUI'),
-            reduxes: path.resolve(__dirname, './src/reduxes'),
-            modules: path.resolve(__dirname, './src/modules'),
-            routers: path.resolve(__dirname, './src/routers'),
-            utils: path.resolve(__dirname, './src/utils'),
-            assets: path.resolve(__dirname, './src/assets'),
-            components: path.resolve(__dirname, './src/components'),
+  context: path.resolve(__dirname, './'),
+  mode: 'production',
+  entry: ['babel-polyfill', path.resolve(__dirname, './src/app.js')],
+  output: {
+    path: path.resolve(__dirname, 'dist/assets'),
+    publicPath: '../assets/',
+    filename: '[name].[chunkhash:8].js',
+    chunkFilename: '[name].[chunkhash:8].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
         },
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+          // use style-loader in development
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.(less|css)$/,
+        use: extractSass.extract({
+          use: ['style-loader', 'css-loader', 'less-loader'],
+        }),
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'url-loader?limit=80000&name=imgs/[hash:8].[ext]',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)$/i,
+        loader: 'url-loader?limit=80000&name=fonts/[hash:8].[ext]',
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          chunks: 'initial',
+          minChunks: 3,
+          name: 'commons',
+          enforce: true,
+        },
+      },
     },
-    externals: {
-        cheerio: 'window',
-        'react/addons': 'react',
-        'react/lib/ExecutionEnvironment': 'react',
-        'react/lib/ReactContext': 'react'
-    }
+  },
+  plugins: [
+    // 插件
+    extractSass,
+    // new webpack.HotModuleReplacementPlugin(),
+    // new webpack.NamedModulesPlugin(),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //     name: 'vendor',
+    //     minChunks: (module) => (
+    //         // 该配置假定你引入的 vendor 存在于 node_modules 目录中
+    //         module.context && module.context.indexOf('node_modules') !== -1
+    //     )
+    // }),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|html)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    new ExtractTextPlugin('styles.[chunkhash:8].css'),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+    }),
+    // // 压缩JS代码,CSS 没有被压缩到
+    // new webpack.optimize.UglifyJsPlugin({
+    //   output: {
+    //     comments: false,
+    //   },
+    //   compress: {
+    //     warnings: false,
+    //     drop_debugger: true,
+    //     drop_console: true,
+    //   },
+    // }),
+    new webpack.IgnorePlugin(/vertx/),
+    new webpack.DefinePlugin({
+      NODE_ENV: process.env.NODE_ENV,
+      __ENV__:
+        process.env.NODE_ENV === 'production'
+          ? prodApiConfig
+          : process.env.NODE_ENV === 'development'
+            ? devApiConfig
+            : process.env.NODE_ENV === 'test'
+              ? testApiConfig
+              : devApiConfig,
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.md', '.txt'],
+    alias: {
+      'react-robotUI': path.resolve(__dirname, './react-robotUI'),
+      reduxes: path.resolve(__dirname, './src/reduxes'),
+      modules: path.resolve(__dirname, './src/modules'),
+      routers: path.resolve(__dirname, './src/routers'),
+      utils: path.resolve(__dirname, './src/utils'),
+      assets: path.resolve(__dirname, './src/assets'),
+      components: path.resolve(__dirname, './src/components'),
+    },
+  },
+  externals: {
+    cheerio: 'window',
+    'react/addons': 'react',
+    'react/lib/ExecutionEnvironment': 'react',
+    'react/lib/ReactContext': 'react',
+  },
 }
